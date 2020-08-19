c19081a5ccf012f2845716ee23fcf049fdbfb81f
lotusGroupXp35 <lotusteamxp35@gmail.com>
2020-08-19 03:15:46 -0300

    Correção do javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 
-const starModal = ()=>{
-    const elementModal = document.querySelector('#modalId')
-    elementModal.classList.toggle('active');
-}
+// const starModal = ()=>{
+//     const elementModal = document.querySelector('#modalId')
+//     elementModal.classList.toggle('active');
+// }
 
-const elementBody = document.querySelector('body')
-elementBody.addEventListener('mouseleave',starModal);
+// const elementBody = document.querySelector('body')
+// elementBody.addEventListener('mouseleave',starModal);
 
